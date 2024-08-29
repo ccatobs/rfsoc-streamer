100b9218cd9cd2924511de97a2e9d013a1dfdfc6
Zach Huber <78050507+zhuber21@users.noreply.github.com>
2024-08-29 11:55:51 -0400

    Update stream_min_example.cxx
    First attempt at a script for running the pipeline in C++

--- scripts/stream_min_example.cxx
+++ scripts/stream_min_example.cxx
@@ -1,18 +1,34 @@
-/* Placeholder file for script that will start a G3Pipeline
- * to catch packets with the minimal working example of
- * the rfsoc-streamer.
-*/
+#include <core/pybindings.h>
+#include <core/G3.h>
+#include <core/G3Pipeline.h>
+#include <core/G3Writer.h>
+#include <RfsocBuilder.h>
+#include <RfsocTransmitter.h>
 
 // Need to include spt3g_software? So3g?
 
+/* Placeholder file for script that will start a G3Pipeline
+ * to catch packets with the minimal working example of
+ * the rfsoc-streamer.
+ * Modeled on sp3g_software/examples/cppexample.cxx
+*/
+
 int main{
 
-    // Generate builder object
+    // Initializing interpreter and releasing GIL
+    // Borrowed from spt3g_software/examples/cppexample.cxx
+    // May need changed for our usage, but will leave for now
+    G3PythonInterpreter interp(false);
 
-    // Generate pipeline
-        // Add builder
-        // Add transmitter on model of DfMuxCollector
-        // Add file writer
-    // Run pipeline
+    G3Pipeline pipe;
+    RfsocBuilder builder;
+    RfsocTransmitter transmitter(builder);
+    transmitter.Start();
 
+    pipe.Add(builder);
+    pipe.Add(G3ModulePtr(new G3Writer("/data/test.g3")));
+
+    pipe.Run();
+
+    return 0;
 }
