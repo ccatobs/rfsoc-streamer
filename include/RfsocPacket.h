7f766ae657c7f74e47fae211487ff4ad5bdc4730
Zach Huber <78050507+zhuber21@users.noreply.github.com>
2024-11-18 16:17:42 -0500

    Packet updates

--- include/RfsocPacket.h
+++ include/RfsocPacket.h
@@ -1,15 +1,15 @@
 #ifndef RFSOCPACKET_H
 #define RFSOCPACKET_H
 
-#define BUFFER_SIZE     8176 // 1022 total possible resonators, 8 bytes each
-#define NUMBER_CHANNELS 2044 // Treating I and Q as separate channels
+#define NUMBER_CHANNELS 2048 // Treating I and Q as separate channels
+// 8212 total bytes in packet - 1024*8 + 20
 
 struct RfsocPacket {
     //Trying immediately casting packets to ints for easier access in RfsocBuilder
-    int32_t data[NUMBER_CHANNELS]; // is this unsigned or signed?
-    unsigned char unused_buffer[6];
-    uint8_t packet_info[4]; // four bytes of flags, one byte per drone
-    uint32_t packet_count;
+    int32_t data[NUMBER_CHANNELS]; 
+    uint8_t packet_info[2]; // two bytes of flags for current drone
+    uint16_t channel_count; // number of tones currently written
+    uint32_t packet_count;  // packet count since streaming began
     unsigned char raw_ptp_timestamp[12];
 } __attribute__((packed));
 
