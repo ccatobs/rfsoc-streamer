a4da8947472b6e96f994024e99dbcb3da025122e
Zach Huber <78050507+zhuber21@users.noreply.github.com>
2025-01-31 21:26:31 -0500

    Initial config file implementation

--- test/stream.py
+++ test/stream.py
@@ -1,22 +1,50 @@
 from spt3g import core
 import ccatrfsoccore # The wrapped C++ library
 import ccatrfsoc     # The pure Python library
-import sys
+import yaml
 import os
-
-# Could eventually load config file if desired
-# This would be a convenient way to set the g3 directory,
-# the IP address of the particular drone, and more.
-
-g3_dir = '/home/streamer/data/g3/'
-source_ip = '192.168.3.58'
-# board ID? Drone ID?
-
-# Stream ID?
-stream_id = 'test'
+import argparse
 
 def main() -> None:
-# Initializing the Python parts of the G3Pipeline
+    if 'STREAM_CONFIG_DIR' in os.environ:
+        cfg_dir = os.environ['STREAM_CONFIG_DIR']
+    else:
+        raise ValueError("STREAM_CONFIG_DIR must be set in environment")
+    
+    with open(os.path.join(cfg_dir, 'stream_config.yml')) as f:
+        cfg = yaml.safe_load(f)
+
+    g3_dir = cfg['g3_dir']
+    file_length = cfg['file_rotation_time']
+
+    # Setting option to pass in board and drone numbers
+    # When running with Docker, these will usually be set in the env
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--board', type=int, default=None,
+                        help='The board number for this streamer instance')
+    parser.add_argument('--drone', type=int, default=None,
+                         help='The drone number for this streamer instance')
+    args = parser.parse_args()
+
+    if 'BOARD' in os.environ:
+        board_num = int(os.environ['BOARD'])
+    elif args.board is not None:
+        board_num = args.board
+    else:
+        raise ValueError("BOARD must be set in env or passed as the 'board' arg")
+
+    if 'DRONE' in os.environ:
+        drone_num = int(os.environ['DRONE'])
+    elif args.drone is not None:
+        drone_num = args.drone
+    else:
+        raise ValueError("DRONE must be set in env or passed as the 'drone' arg")
+
+    # The Stream ID tells you which board and drone the data is coming from
+    stream_id = cfg['rfsocs'][f'BOARD[{board_num}]'][f'DRONE[{drone_num}]']['stream_id']
+    source_ip = cfg['rfsocs'][f'BOARD[{board_num}]'][f'DRONE[{drone_num}]']['drone_stream_ip']
+
+    # Initializing the Python parts of the G3Pipeline
     # Should eventually add the ability to set the variables
     # that are passed by default to the C++ RfsocBuilder constructor (debug, etc.)
     builder = ccatrfsoccore.RfsocBuilder()
@@ -26,7 +54,7 @@ def main() -> None:
     pipe = core.G3Pipeline()
     pipe.Add(builder)
     pipe.Add(ccatrfsoc.SessionManager.SessionManager, stream_id=stream_id)
-    pipe.Add(ccatrfsoc.G3Rotator, data_path=g3_dir, file_dur=60)
+    pipe.Add(ccatrfsoc.G3Rotator, data_path=g3_dir, file_dur=file_length)
     pipe.Run(profile=False)
 
 if __name__ == '__main__':
